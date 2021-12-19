064e6723c684a7f53bb3bdd211804ca5a693ce26
Francisco Beltrao <frbeltra@microsoft.com>
2021-12-19 17:13:24 +0100

    Fix lint errors

--- src/function_one/tests/test_get_company_details.py
+++ src/function_one/tests/test_get_company_details.py
@@ -1,14 +1,16 @@
-
+"""Tests for get_company_details function"""
 import json
 import azure.functions as func
 from get_company_details import main
 
+
 def test_get_company_details():
+    """Test get_company_details function"""
     # Arrange
     req = func.HttpRequest(
-        method='GET',
-        url='/api/get-company-details',
-        body=None,   
+        method="GET",
+        url="/api/get-company-details",
+        body=None,
     )
 
     # Act
@@ -16,4 +18,7 @@ def test_get_company_details():
 
     # Assert
     assert res.status_code == 200
-    assert json.loads(str(res.get_body(), "utf-8")) == { "name": "Contoso", "address": "Contosostrasse 1, Zurich, Switzerland" }
+    assert json.loads(str(res.get_body(), "utf-8")) == {
+        "name": "Contoso",
+        "address": "Contosostrasse 1, Zurich, Switzerland",
+    }
