43253e55c20f4743d346ee196c8b7c2a944f4a5a
Francisco Beltrao <frbeltra@microsoft.com>
2021-12-24 15:20:08 +0100

    Format py files

--- src/function_one/get_company_details/__init__.py
+++ src/function_one/get_company_details/__init__.py
@@ -6,7 +6,7 @@ import azure.functions as func
 from contoso import get_company_address, get_company_name
 
 
-#pylint: disable=unused-argument
+# pylint: disable=unused-argument
 def main(req: func.HttpRequest) -> func.HttpResponse:
     """Get company details"""
     logging.info("Python HTTP trigger function processed a request.")
