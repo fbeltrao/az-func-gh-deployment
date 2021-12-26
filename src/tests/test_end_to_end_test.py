c7d9ac9f749a89450f154641d084e9d14d1d5787
Francisco Beltrao <frbeltra@microsoft.com>
2021-12-26 11:01:37 +0100

    Run e2e tests in Stage (#10)
    * Run e2e tests in Stage

--- src/tests/test_end_to_end_test.py
+++ src/tests/test_end_to_end_test.py
@@ -1,18 +1,22 @@
+import os
+from typing import Union
 import pytest
 import requests
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
 @pytest.fixture(scope="session")
-def function_one_url():    
-    function_name = "<get-the-function-name>"
-    function_code = "<get-the-function-code>"
-    return f"https://{function_name}.azurewebsites.net/api/get_company_details?code={function_code}"
+def function_one_url() -> Union[str, None]:
+    function_app_one_name = os.getenv("E2E_TESTS_function_app_one_name")
+    function_app_one_key = os.getenv("E2E_TESTS_function_app_one_key")
+    if function_app_one_name and function_app_one_key:
+        return f"https://{function_app_one_name}.azurewebsites.net/api/get_company_details?code={function_app_one_key}"
+    return None
 
 def test_can_call_function_one(function_one_url: str) -> None:
 
     if not function_one_url:
-        pytest.skip("No function_one_url provided")
+        pytest.skip("URI and code for function one were not provided")
 
     # Act
     retry_strategy = Retry(
